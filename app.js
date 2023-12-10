5db6af797cb3d7e7804e6e8ccc835c229d117f71
alex <nba4everwanted@gmail.com>
2023-12-10 08:25:39 -0500

    Adding data to app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+//Global Variable
+
+// __dirname
+// __filename
+// require
+// module
+// process
+
 //Modules are (Encapsulated Code) ie Will only share what we want (the minimum)
-const names = require('./4-names');
-const sayHi = require('./5-utils');
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative_flavore")
 
 //console.log(sayHi);
-
+console.log(data)
 sayHi("Luke");
 sayHi(names.john);
 sayHi(names.peter);
